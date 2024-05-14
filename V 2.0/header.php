dcc661aaad1df61a062d36d5c023bc0ab64c293e
Patryk <siekpat@proton.me>
2024-05-14 14:59:30 +0100

    Adding 2.0 version of website

---
+++ V 2.0/header.php
@@ -0,0 +1,19 @@
+<?php
+    echo '<!DOCTYPE HTML> -
+        <html lang="pl"> -
+        <head> -
+            <meta charset="utf-8"/> -
+            <title>World\'s herbs</title> -
+            <meta name="description" content="Website about herbs."/> -
+            <meta name=keywords" content="herbology herb, herbs, world\'s herbs"/> -
+            <meta http-equiv="X-UA-Compatible" content="IE-edge,chrome=1"/> -
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/> -
+            <link rel="stylesheet" href="style.css" type="text/css"/> -
+        </head> -
+        <body> -
+            <div id = "container"> -
+                <a href="index.html"><div id = "logo"><img src = "img/logo.png"/></div></a> -
+                <div id = "definition"> -
+                    <p><b>Zielarstwo</b> - Dział ogrodnictwa zajmujący się uprawą ziół.</p> -
+                </div>'
+?>
